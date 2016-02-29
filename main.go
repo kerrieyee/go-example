ccd69d1f6e6724b4ac2ce69f0ad05c3ca3271a71
Kerrie Yee <kerrieyee@Kerries-MacBook-Pro.local>
2016-02-28 21:56:25 -0800

    with a database hooked up

--- main.go
+++ main.go
@@ -1,33 +1,216 @@
 package main
 
 import (
-    "html/template"
+    "database/sql"
+    "encoding/json"
+    "log"
     "net/http"
-    "path"
+    "fmt"
+
+    "github.com/julienschmidt/httprouter"
+    _ "github.com/mattn/go-sqlite3"
 )
 
 type book struct {
-    Title  string
-    Author string
+    ID     string `json:"id"`
+    Author string `json:"author"`
+    Title  string `json:"title"`
 }
 
-var fp = path.Join("templates", "index.html")
-var tmpl, err = template.ParseFiles(fp)
+type errResponse struct {
+    Message string `json:"message"`
+}
 
 func main() {
-    http.HandleFunc("/", showBooks)
-    http.ListenAndServe(":8080", nil)
-}
-
-func showBooks(w http.ResponseWriter, r *http.Request) {
-    b := book{"Building Web Apps with Go", "Jeremy Saenz"}
-
+    db, err := newDB()
     if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
+        log.Fatalln("Could not connect to database")
     }
 
-    if err := tmpl.Execute(w, b); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
+    r := httprouter.New()
+    r.GET("/books", getHandler(db))
+    r.POST("/books", postHandler(db))
+    r.DELETE("/books/:id", deleteHandler(db))
+    r.GET("/books/:id", showHandler(db))
+
+
+    log.Println("Listening on :8080")
+    http.ListenAndServe(":8080", r)
+}
+
+func getHandler(db *sql.DB) httprouter.Handle {
+    return func(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+        books, err := getBooks(db)
+        if err != nil {
+            respondError(rw, err)
+            return
+        }
+
+        rw.Header().Set("Content-Type", "application/json")
+
+        if err := json.NewEncoder(rw).Encode(books); err != nil {
+            respondError(rw, err)
+            return
+        }
+    }
+}
+
+func showHandler(db *sql.DB) httprouter.Handle {
+    return func(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+        books, err := getBook(db, p)
+        if err != nil {
+            respondError(rw, err)
+            return
+        }
+
+        rw.Header().Set("Content-Type", "application/json")
+
+        if err := json.NewEncoder(rw).Encode(books); err != nil {
+            respondError(rw, err)
+            return
+        }
+    }
+}
+
+func deleteHandler(db *sql.DB) httprouter.Handle {
+    return func(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+        books, err := deleteBook(db, p)
+        if err != nil {
+            respondError(rw, err)
+            return
+        }
+
+        rw.Header().Set("Content-Type", "application/json")
+
+        if err := json.NewEncoder(rw).Encode(books); err != nil {
+            respondError(rw, err)
+            return
+        }
+    }
+}
+
+func postHandler(db *sql.DB) httprouter.Handle {
+    return func(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+        var b book
+        if err := json.NewDecoder(r.Body).Decode(&b); err != nil {
+            respondError(rw, err)
+            return
+        }
+        fmt.Println(b)
+        if err := createBook(db, b); err != nil {
+            respondError(rw, err)
+            return
+        }
+
+        rw.WriteHeader(http.StatusNoContent)
+    }
+}
+
+
+func newDB() (*sql.DB, error) {
+    db, err := sql.Open("sqlite3", "example.sqlite")
+    if err != nil {
+        log.Println(err)
+        return nil, err
+    }
+
+    q := "CREATE TABLE IF NOT EXISTS books(id INT, title TEXT, author TEXT)"
+    if _, err := db.Exec(q); err != nil {
+        log.Println(err)
+        return nil, err
+    }
+
+    return db, nil
+}
+
+func getBook(db *sql.DB, p httprouter.Params) ([]book, error) {
+    id := p.ByName("id")
+    q := "SELECT * FROM books where id=" + id
+    rows, err := db.Query(q)
+    if err != nil {
+        log.Println(err)
+        return nil, err
+    }
+
+    var books []book
+    for rows.Next() {
+        var b book
+        if err := rows.Scan(&b.ID, &b.Title, &b.Author); err != nil {
+            log.Println(err)
+            return nil, err
+        }
+
+        books = append(books, b)
+    }
+
+    return books, nil
+}
+
+func deleteBook(db *sql.DB, p httprouter.Params) ([]book, error) {
+    id := p.ByName("id")
+    q := "DELETE FROM books where id=" + id
+    rows, err := db.Query(q)
+    if err != nil {
+        log.Println(err)
+        return nil, err
+    }
+
+    var books []book
+    for rows.Next() {
+        var b book
+        if err := rows.Scan(&b.ID, &b.Title, &b.Author); err != nil {
+            log.Println(err)
+            return nil, err
+        }
+
+        books = append(books, b)
+    }
+
+    return books, nil
+}
+
+func getBooks(db *sql.DB) ([]book, error) {
+    q := "SELECT id, title, author FROM books"
+    rows, err := db.Query(q)
+    if err != nil {
+        log.Println(err)
+        return nil, err
+    }
+
+    var books []book
+    for rows.Next() {
+        var b book
+        if err := rows.Scan(&b.ID, &b.Title, &b.Author); err != nil {
+            log.Println(err)
+            return nil, err
+        }
+
+        books = append(books, b)
+    }
+
+    return books, nil
+}
+
+func createBook(db *sql.DB, b book) error {
+
+    q := "INSERT INTO books(id, title, author) VALUES ($1, $2, $3)"
+    if _, err := db.Exec(q, b.ID, b.Title, b.Author); err != nil {
+        log.Println(err)
+        return err
+    }
+
+    return nil
+}
+
+func respondError(rw http.ResponseWriter, err error) {
+    rw.Header().Set("Content-Type", "application/json")
+    rw.WriteHeader(http.StatusInternalServerError)
+
+    er := errResponse{
+        Message: err.Error(),
+    }
+
+    if err := json.NewEncoder(rw).Encode(er); err != nil {
+        log.Println(err)
     }
 }
